cfc2b39a16d9d0f3b8e4033e0603f3a18778cd58
Pablo Vicente <pablo.vicente@cybus.io>
2024-04-11 09:49:03 +0200

    Add support for MTLS, and other MQTT options

--- go.mod
+++ go.mod
@@ -2,15 +2,20 @@ module github.com/pablitovicente/mqtt-to-nats
 
 go 1.19
 
-require github.com/eclipse/paho.mqtt.golang v1.4.2
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.2
+	github.com/nats-io/nats.go v1.33.1
+	github.com/pablitovicente/mqtt-load-generator v1.0.6
+)
 
 require (
-	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/nats-io/nats.go v1.20.0 // indirect
-	github.com/nats-io/nkeys v0.3.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/nats-io/nkeys v0.4.7 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
-	github.com/pablitovicente/mqtt-load-generator v1.0.1 // indirect
-	golang.org/x/crypto v0.0.0-20210314154223-e6e6c4f2bb5b // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
--- main.go
+++ main.go
@@ -19,6 +19,14 @@ func main() {
 	host := flag.String("h", "localhost", "MQTT host")
 	port := flag.Int("p", 1883, "MQTT port")
 	qos := flag.Int("q", 1, "MQTT QoS used by all clients")
+	cert := flag.String("cert", "", "Path to TLS certificate file")
+	ca := flag.String("ca", "", "Path to TLS CA file")
+	key := flag.String("key", "", "Path to TLS key file")
+	insecure := flag.Bool("insecure", false, "Set to true to allow self signed certificates")
+	mqtts := flag.Bool("mqtts", false, "Set to true to use MQTTS")
+	cleanSession := flag.Bool("cleanSession", true, "Set to true for clean MQTT sessions or false to keep session")
+	clientID := flag.String("clientID", "mqtt-to-nats-bridge", "Custom MQTT clientID")
+	keepAliveTimeout := flag.Int64("keepAliveTimeout", 5000, "Set the amount of time (in seconds) that the client should wait before sending a PING request to the broker")
 	natsURL := flag.String("N", "nats://localhost:4222", "NATS Stream server url for example nats://localhost:4222")
 	natsStreamName := flag.String("SN", "collector", "NATS Stream name used to store MQTT forwarded messages")
 	natsStreamReplicas := flag.Int("R", 1, "Number of NATS Stream replicas")
@@ -42,19 +50,30 @@ func main() {
 
 	// General Client Config
 	mqttClientConfig := MQTTClient.Config{
-		TargetTopic: targetTopic,
-		Username:    username,
-		Password:    password,
-		Host:        host,
-		Port:        port,
-		QoS:         qos,
+		TargetTopic:      targetTopic,
+		Username:         username,
+		Password:         password,
+		Host:             host,
+		Port:             port,
+		QoS:              qos,
+		CleanSession:     cleanSession,
+		ClientID:         clientID,
+		KeepAliveTimeout: keepAliveTimeout,
+		Insecure:         insecure,
+		MQTTS:            mqtts,
+	}
+
+	if TLSOptionsSet() {
+		mqttClientConfig.TLSConfigured = true
+		mqttClientConfig.CA = ca
+		mqttClientConfig.Cert = cert
+		mqttClientConfig.Key = key
 	}
 
 	rand.Seed(time.Now().UnixNano())
 	updates := make(chan int)
 
 	mqttClient := MQTTClient.Client{
-		ID:      rand.Intn(100000),
 		Config:  mqttClientConfig,
 		Updates: updates,
 	}
@@ -64,7 +83,7 @@ func main() {
 	nc, _ := nats.Connect(*natsURL)
 	js, _ := nc.JetStream(nats.PublishAsyncMaxPending(*maxInflightMessages))
 
-	streamInfo, err := js.AddStream(&nats.StreamConfig{
+	_, err := js.AddStream(&nats.StreamConfig{
 		Name:     *natsStreamName,
 		Subjects: []string{*natsStreamName},
 		Replicas: *natsStreamReplicas,
@@ -74,7 +93,6 @@ func main() {
 	if err != nil {
 		fmt.Println("Error creating NATS Stream:", err)
 	}
-	fmt.Println("Stream info", streamInfo)
 
 	mqttClient.Connection.Subscribe(*targetTopic, byte(*qos), func(c mqtt.Client, m mqtt.Message) {
 		_, err := js.PublishAsync(*natsStreamName, m.Payload())
@@ -84,4 +102,26 @@ func main() {
 	})
 
 	select {}
+}
+
+func TLSOptionsSet() bool {
+	foundCert := false
+	foundCA := false
+	foundKey := false
+
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == "cert" {
+			foundCert = true
+		}
+
+		if f.Name == "ca" {
+			foundCA = true
+		}
+
+		if f.Name == "key" {
+			foundKey = true
+		}
+	})
+
+	return foundCA && foundCert && foundKey
 }
